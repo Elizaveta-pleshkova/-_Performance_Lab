dd22a34c40a9a9e10090e1f772f70547ebac57dc
Elizaveta_Pleshova <lisavet02@gmail.com>
2024-12-20 14:59:54 +0400

    finish commit

--- task4/task4.py
+++ task4/task4.py
@@ -1,24 +1,36 @@
 import statistics
+import sys
+import argparse
 
-path_numbers = input("Enter the default path of the Numbers file: ")
-file_num = open(path_numbers, "rt")
-arr_file_num = file_num.readlines()
-file_num.close()
 
-arr_num = []
-for i in arr_file_num:
-    arr_num.append(int(i))
+def create_path_numbers():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', nargs='?')
 
-median = statistics.median_high(sorted(arr_num))
+    return parser
 
-count = 0
-for i in arr_num:
-    while i != median:
-        if i > median:
-            i -= 1
-            count += 1
-        else:
-            i += 1
-            count += 1
 
-print(count)
+if __name__ == '__main__':
+    parser = create_path_numbers()
+    path_numbers = parser.parse_args()
+    file_num = open(path_numbers.path, "rt")
+    arr_file_num = file_num.readlines()
+    file_num.close()
+
+    arr_num = []
+    for i in arr_file_num:
+        arr_num.append(int(i))
+
+    median = statistics.median_high(sorted(arr_num))
+
+    count = 0
+    for i in arr_num:
+        while i != median:
+            if i > median:
+                i -= 1
+                count += 1
+            else:
+                i += 1
+                count += 1
+
+    print(count)
